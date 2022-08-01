a19cf587366839e2a6da4105caf0c343ca1aa8bd
Ryan <aarchaeopteryxx@gmail.com>
2022-08-01 12:58:44 -0700

    #7 Basic test setup & #8 Postgres setup (#16)
    * add db, user schema, base env
    
    * tweak scripts
    
    * add workflows and web test
    
    * fix lint, add pretty
    
    * npx'd eslint
    
    * update readme and env defaults
    
    * emojis
    
    * update workflow script

--- README.md
+++ README.md
@@ -2,27 +2,40 @@
 
 This is a template application using RedwoodJS. It establishes common patterns for creating an app quickly.
 
-## Development Setup
+## 🧱 Development Setup
 
 > **Prerequisites**
 >
 > - Redwood requires [Node.js](https://nodejs.org/en/) (>=14.19.x <=16.x) and [Yarn](https://yarnpkg.com/) (>=1.15)
 > - Are you on Windows? For best results, follow our [Windows development setup](https://redwoodjs.com/docs/how-to/windows-development-setup) guide
 
-Start by installing dependencies:
+## ⚡️ Quickstart
 
-```shell
+```bash
+# install dependencies
 yarn install
+
+# setup .env
+cp .env.example .env
+
+# start dev server and database
+# Your browser should automatically open to <http://localhost:8910>.
+yarn dev
+
+# (first run) run command in a separate terminal to setup database and seed data
+yarn db:setup
+
+# See extra helper scripts in package.json
+
 ```
 
-Then start the development server:
+## 🔍 Testing
 
-```shell
-yarn redwood dev
+```bash
+yarn test # <api|web>
+yarn test:watch # <api|web>
 ```
 
-Your browser should automatically open to <http://localhost:8910>.
-
-## Github Templates
+## 📄 Github Templates
 
 To make creating new issues and pull requests easy and consistent, we have templates for the different use cases when creating an issue or pull request. You can find them in the `.github` directory.
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,40 +1,71 @@
-import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
+import Chance from 'chance'
+import CryptoJS from 'crypto-js'
+
+function hashPassword(password: string, salt = 'ZEAL') {
+  const saltToUse = salt || CryptoJS.lib.WordArray.random(128 / 8).toString()
+  return [
+    CryptoJS.PBKDF2(password, saltToUse, { keySize: 256 / 32 }).toString(),
+    saltToUse,
+  ]
+}
+
+const {
+  ADMIN_EMAIL = 'admin@example.com',
+  ADMIN_PASSWORD = 'password',
+  USERS_PASSWORD = 'password',
+} = process.env
+
+const [ADMIN_HASHED_PASSWORD, ADMIN_SALT] = hashPassword(ADMIN_PASSWORD)
+const [USERS_HASHED_PASSWORD, USERS_SALT] = hashPassword(USERS_PASSWORD)
+
+const chance = new Chance('ZEAL')
+
+function pickRandom(arr: string[]) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function generateUser(override?: Record<string, unknown>) {
+  return {
+    name: chance.name(),
+    nickname: chance.word(),
+    pronouns: pickRandom(['he/him', 'she/her', 'they/them', 'it/its']),
+    email: chance.email(),
+    hashedPassword: USERS_HASHED_PASSWORD,
+    salt: USERS_SALT,
+    ...override,
+  }
+}
+
+async function _upsertUser(user) {
+  return db.user.upsert({
+    where: {
+      email: user.email,
+    },
+    update: {
+      ...user,
+    },
+    create: {
+      ...user,
+    },
+  })
+}
 
 export default async () => {
   try {
-    //
-    // Manually seed via `yarn rw prisma db seed`
-    // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
-    //
-    // Update "const data = []" to match your data model and seeding needs
-    //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
+    const users = [
+      generateUser({
+        admin: true,
+        email: ADMIN_EMAIL,
+        hashedPassword: ADMIN_HASHED_PASSWORD,
+        salt: ADMIN_SALT,
+      }),
+      ...Array(5).map(() => generateUser()),
     ]
-    console.log(
-      "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
-    )
-
-    // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
-    // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
-    Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
-      })
-    )
-  } catch (error) {
-    console.warn('Please define your seed data.')
-    console.error(error)
+    await Promise.all(users.map(_upsertUser))
+    return null
+  } catch (err) {
+    console.error(err.message)
+    return null
   }
 }
