96254c67f7d2ed643634416487ce7d821da2998a
Zan Rosenthal <zan.rosenthal@gmail.com>
2022-08-12 16:47:37 +0200

    Set up auth0

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -8,16 +8,30 @@ const LinkItem = (props) => (
 )
 
 const Navigation = () => {
-  const { logOut, isAuthenticated } = useAuth()
+  const { logIn, logOut, isAuthenticated } = useAuth()
+
+  const handleAuthButtonClick = async () => {
+    if (isAuthenticated) {
+      await logOut({ returnTo: process.env.AUTH0_REDIRECT_URI })
+    } else {
+      const searchParams = new URLSearchParams(window.location.search)
+      await logIn({
+        appState: { targetUrl: searchParams.get('redirectTo') },
+      })
+    }
+  }
 
   return (
     <ul data-testid="nav" className="my-3 flex flex-row items-start">
       <LinkItem to={routes.home()}>Home</LinkItem>
-      {isAuthenticated ? (
+      <button onClick={handleAuthButtonClick}>
+        {isAuthenticated ? 'Log out' : 'Log in'}
+      </button>
+      {/* {isAuthenticated ? (
         <LinkItem onClick={logOut}>Logout</LinkItem>
       ) : (
         <LinkItem to={routes.login()}>Login</LinkItem>
-      )}
+      )} */}
     </ul>
   )
 }
