f5ef52d635ae4d4bb62a36727f9e06a2e28ff566
Natalia Diaz <101531284+nataliadiaz2@users.noreply.github.com>
2022-09-08 11:49:26 -0700

    🧹 Forbidden Page Update (#65)

--- web/src/pages/ForbiddenPage/ForbiddenPage.tsx
+++ web/src/pages/ForbiddenPage/ForbiddenPage.tsx
@@ -1,13 +1,44 @@
-import { MetaTags } from '@redwoodjs/web'
-
-const ForbiddenPage = () => {
-  return (
-    <>
-      <MetaTags title="Forbidden" description="Forbidden" />
-
-      <h1>403 Forbidden</h1>
-    </>
-  )
-}
-
-export default ForbiddenPage
+export default () => (
+  <main>
+    <style
+      dangerouslySetInnerHTML={{
+        __html: `
+              html, body {
+                margin: 0;
+              }
+              html * {
+                box-sizing: border-box;
+              }
+              main {
+                display: flex;
+                align-items: center;
+                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif;
+                text-align: center;
+                background-color: #E2E8F0;
+                height: 100vh;
+              }
+              section {
+                background-color: white;
+                border-radius: 0.25rem;
+                width: 32rem;
+                padding: 1rem;
+                margin: 0 auto;
+                box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+              }
+              h1 {
+                font-size: 2rem;
+                margin: 0;
+                font-weight: 500;
+                line-height: 1;
+                color: #2D3748;
+              }
+            `,
+      }}
+    />
+    <section>
+      <h1>
+        <span>403 Forbidden</span>
+      </h1>
+    </section>
+  </main>
+)
