f1bbf02d04a77c55bc961e669c461bc56732a6e4
nataliadiaz2 <natdiaz2001@gmail.com>
2022-09-16 08:37:08 -0700

    Footer Add

--- web/src/layouts/MainLayout.tsx
+++ web/src/layouts/MainLayout.tsx
@@ -1,10 +1,32 @@
+import { getYear } from 'date-fns'
+
 import { Navigation } from '../components/Navigation'
+
+const getCurrentYear = (): string => {
+  const year = getYear(new Date(Date.now())).toString()
+  return year
+}
 
 const MainLayout = ({ children }) => {
   return (
     <div className="m-4">
       <Navigation />
       {children}
+      <footer
+        className="text-center text-sm italic text-gray-500"
+        data-testid="copyright"
+      >
+        Copyright &copy;{getCurrentYear()}.{' '}
+        <a
+          href="http://codingzeal.com"
+          target="_blank"
+          className="underline"
+          rel="noreferrer"
+        >
+          Coding ZEAL
+        </a>
+        . All Rights Reserved.
+      </footer>
     </div>
   )
 }
