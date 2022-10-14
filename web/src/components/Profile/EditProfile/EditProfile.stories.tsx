040c0575d9661412618191f5ab95287b4410b3c3
nataliadiaz2 <natdiaz2001@gmail.com>
2022-10-14 11:47:05 -0700

    Add unit tests and stories for profile forms

--- web/src/components/Profile/EditProfile/EditProfile.stories.tsx
+++ web/src/components/Profile/EditProfile/EditProfile.stories.tsx
@@ -1,7 +1,19 @@
 import { EditProfile } from './EditProfile'
 
-export const generated = (args) => {
+const Template = (args) => {
   return <EditProfile {...args} />
 }
+
+export const EmptyProfile = Template.bind({})
+EmptyProfile.args = { profile: {} }
+
+const profile = {
+  name: 'Mulder',
+  nickname: 'Spooky Mulder',
+  pronouns: 'he/him',
+  email: 'mulder@example.com',
+}
+export const FilledProfile = Template.bind({})
+FilledProfile.args = { profile }
 
 export default { title: 'Components/Profile/EditProfile' }
