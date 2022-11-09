f6bb6e3f45015b03c09ed5a4ec2ab15a8f3521d6
natalia <natdiaz2001@gmail.com>
2022-11-08 16:21:34 -0800

    WIP Admin e2e

--- web/tests/admin-user.spec.ts
+++ web/tests/admin-user.spec.ts
@@ -19,15 +19,34 @@ test.beforeEach(async ({ page }) => {
 
   const logout = await page.locator('text=Logout')
   await expect(logout).toBeVisible()
-  const admin = await page.locator('text=Admin')
+  const admin = await page.locator('text=Admin').first()
   await expect(admin).toBeVisible()
 
-  await page.locator('text=Admin').click()
+  await page.locator('text=Admin').first().click()
   await page.waitForURL('/admin/users')
 })
 
-test.describe('admin crud user', () => {
-  test('should save profile changes', async ({ page }) => {})
+const MOCK_USER = {
+  email: 'endtoend@example.com',
+  name: 'E2E',
+  nickname: 'E2ENickName',
+  pronouns: 'E2EPronouns',
+}
 
-  test('should update profile name in navigation', async ({ page }) => {})
+test.describe('admin crud user', () => {
+  test('admin creates a new user', async ({ page }) => {
+    await page.locator('text=New User').click()
+    await page.waitForURL('/admin/users/new')
+    await page.locator('input[name="email"]').click()
+    await page.locator('input[name="email"]').fill(MOCK_USER.email)
+    await page.locator('input[name="name"]').click()
+    await page.locator('input[name="name"]').fill(MOCK_USER.name)
+    await page.locator('input[name="nickname"]').click()
+    await page.locator('input[name="nickname"]').fill(MOCK_USER.nickname)
+    await page.locator('input[name="pronouns"]').click()
+    await page.locator('input[name="pronouns"]').fill(MOCK_USER.pronouns)
+
+    await page.locator('button:has-text("Save")').click()
+    await page.waitForURL('/admin/users')
+  })
 })
