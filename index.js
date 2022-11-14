36ef53cd5f5bad0ac72ea02d3ec5295dd86571a1
Ryan <thefakeryan@icloud.com>
2022-11-14 13:50:38 -0800

    remove socket

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ pm2.start(
     name: 'redwood-template-app',
     node_args: '-r dotenv/config',
     script: './node_modules/.bin/redwood',
-    args: `serve api -p ${process.env.PORT || 8911} --socket /tmp/nginx.socket`,
+    args: `serve api -p ${process.env.PORT || 8911}`,
     env: {
       NODE_ENV: 'production',
     },
