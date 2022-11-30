909d9897d0f56d058ce26981bc80b6d6d768b6d9
Eddie Gómez <41875751+iameddieyayaya@users.noreply.github.com>
2022-11-30 11:57:06 -0800

    fix safari cookie issue with secure header (#141)
    * fix safari cookie issue with secure header
    
    * remove redundant yarn rw build

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -38,7 +38,7 @@ const config: PlaywrightTestConfig = {
   ],
   webServer: {
     reuseExistingServer: true,
-    command: 'yarn rw dev',
+    command: 'yarn rw serve',
     port: 8910,
     env: {
       DISABLE_EMAIL: 'true',
