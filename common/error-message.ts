a655bc6c2103ea2205e554b17db4ad9fc2d1300c
natalia <natdiaz2001@gmail.com>
2022-12-12 09:20:24 -0800

    WIP API/Web link

--- common/error-message.ts
+++
@@ -1,1 +0,0 @@
-export const verifyMessageError = 'User not Verified'
---
+++ common/hello.ts
@@ -0,0 +1,6 @@
+const world = 'world'
+
+export default function hello(who: string = world): string {
+  console.log('cnwcwnkskmxksm')
+  return `Hello ${who}! `
+}
