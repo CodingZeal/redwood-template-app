bc184b8839eed97a6aca9d0557dc9af4744052ee
archae0pteryx <github@pocketcereal.com>
2023-01-03 14:32:29 -0800

    create conflicts

--- index.js
+++ index.js
@@ -15,18 +15,6 @@ pm2.start(
     },
   },
   function (err) {
-    if (err)
-      return console.error(
-        'Error while launching applications',
-        err.stack || err
-      )
-
-    console.log('[+] PM2 Started!')
-
-    if (process.env.DYNO) {
-      console.log(`[+] Sending ready signal to NGINX`)
-      fs.openSync('/tmp/app-initialized', 'w')
-    }
 
     pm2.launchBus((err, bus) => {
       console.log('[PM2] log stream started')
@@ -39,5 +27,18 @@ pm2.start(
         console.error(`[App:${packet.process.name}][ERR!] ${packet.data}`)
       })
     })
+        if (err)
+      return console.error(
+        'Error while launching applications',
+        err.stack || err
+      )
+
+    console.log('[+] PM2 Started!')
+
+    if (process.env.DYNO) {
+      console.log(`[+] Sending ready signal to NGINX`)
+      fs.openSync('/tmp/app-initialized', 'w')
+    }
+
   }
 )
