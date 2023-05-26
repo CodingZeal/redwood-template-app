15698fd284d9340a0644dad5881065756c0ed8f5
Erik Guzman <erik.guzman@codingzeal.com>
2023-05-26 13:43:25 -0700

    Update pw reset user setup to store hash token

--- web/tests/reset-password.spec.ts
+++ web/tests/reset-password.spec.ts
@@ -5,9 +5,10 @@ import { MockUserEntity } from './entities/user.entity'
 test.beforeAll(async () => {
   await MockUserEntity.upsert({
     email: 'snap@crackle.pop',
-    resetToken: 'waffleCrisp',
+    resetToken:
+      '013ce53f5abe360066909e76d37ef32111da99e13591db3ac4aa3857f5a20abf',
     resetTokenExpiresAt: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
-    verifyToken: 'cheerios',
+    verifyToken: 'waffleCrisp',
   })
 })
 
